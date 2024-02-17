b7c04fae6ac83cabe499e7c35cdba4207094bda6
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2024-02-17 00:11:10 -0300

    test: adicionando teste para a criação de vetores

--- tests/test_vector_creator.c
+++ tests/test_vector_creator.c
@@ -1,8 +1,9 @@
 #include "utils.h"
 
+extern t_memory *memory;
 
 void teardown(void) {
-    ft_printf("FInalizando\n");
+    memory->memory_flush(ALL);
 }
 
 Test(vector, vector_new)
@@ -14,6 +15,22 @@ Test(vector, vector_new)
     cr_assert_float_eq(vector->x, 1.0, EPSILON);
     cr_assert_float_eq(vector->y, 2.0, EPSILON);
     cr_assert_float_eq(vector->z, 3.0, EPSILON);
+}
+
+Test(vector, vsum) 
+{
+    t_vector* v1; 
+    t_vector* v2;
+    t_vector* v3;
+
+    v1 = vector_new(1,1,1); 
+    v2 = vector_new(1,1,1);
+
+    v3 = vsum(v1, v2);
+
+    cr_assert_float_eq(v3->x, 2.0, EPSILON);
+    cr_assert_float_eq(v3->y, 2.0, EPSILON);
+    cr_assert_float_eq(v3->z, 2.0, EPSILON);
 
 }
 
