2c0d334d139780cde6f83071a6168f52b2e8f0d8
JoaoVictorRosmaninho <joaovictorrosmaninho@gmail.com>
2024-03-17 20:16:21 -0300

    segregate in files

---
+++ minirt_fix/memory/arena_align_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arena_align_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jv <jv@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 18:20:04 by jv                #+#    #+#             */
+/*   Updated: 2024/03/17 20:03:25 by jv               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./arena.h"
+
+size_t	ft_arena_normalizer(size_t chunk)
+{
+	chunk--;
+	chunk |= chunk >> 1;
+	chunk |= chunk >> 2;
+	chunk |= chunk >> 4;
+	chunk |= chunk >> 8;
+	chunk |= chunk >> 16;
+	return (++chunk);
+}
+
+size_t	ft_align(size_t request_size)
+{
+	return (((request_size) + ((ARENA_ALIGN_SIZE) - 1))
+		& ~((ARENA_ALIGN_SIZE) - 1));
+}
